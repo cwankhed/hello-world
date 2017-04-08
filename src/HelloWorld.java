0385b0926702a2497ce4e26c036a838d79559895
Chaitanya Wankhede <chaitanya_wankhede@yahoo.com>
2017-04-09 00:26:38 +0530

    something new
    Signed-off-by: Chaitanya Wankhede <chaitanya_wankhede@yahoo.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Say hello to the new program world!");
+		System.out.println("Say hello to the new program world!.. something new");
 	}
 
 }
